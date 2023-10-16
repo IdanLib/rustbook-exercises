7fcf14c4a0483f773be9ec34c395842b92806f58
idanl <idanlib@gmail.com>
2023-10-16 23:46:46 +0300

    rust server

--- chapter-20-server/src/main.rs
+++ chapter-20-server/src/main.rs
@@ -1,4 +1,18 @@
-use std::net::TcpListener; // Built-in module to listen to TCP streams
+use std::{
+    net::{TcpListener, TcpStream}, 
+    io::{BufRead, BufReader}
+}; // Built-in module to listen to TCP streams
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+}
+ 
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap(); // Returns a new TcpListener instance
@@ -6,6 +20,7 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        // println!("Connection established!");
+        handle_connection(stream);
     }
 }
