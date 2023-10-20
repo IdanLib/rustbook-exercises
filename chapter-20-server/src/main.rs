ddf4d1e14c68060afcbace3d6a3642f3e4e00dea
idanl <idanlib@gmail.com>
2023-10-20 23:39:36 +0300

    server sleep

--- chapter-20-server/src/main.rs
+++ chapter-20-server/src/main.rs
@@ -1,21 +1,33 @@
 use std::{
     net::{TcpListener, TcpStream}, 
-    io::{BufRead, BufReader, Write}, fs
+    io::{BufRead, BufReader, Write}, fs, thread, time::Duration
 }; // Built-in module to listen to TCP streams
 
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // let full_http_request: Vec<_> = buf_reader
+    //     .lines()
+    //     .map(|result| result.unwrap())
+    //     .take_while(|line| !line.is_empty())
+    //     .collect();
 
-    // println!("{:#?}", http_request);
-    
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let request_header = buf_reader
+        .lines()
+        .next()
+        .unwrap()
+        .unwrap();
+
+    println!("{:#?}", request_header);
+
+    let (status_line, filename) = if request_header.contains(" / ") {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+   
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
     let response = 
         format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
@@ -30,7 +42,6 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        // println!("Connection established!");
         handle_connection(stream);
     }
 }
